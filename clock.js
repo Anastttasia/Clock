9c8b35a31d4eedf3bfd7b1285f041dc2f189cf8d
Anastttasia <nastya.kaneva.99@gmail.com>
2022-05-25 09:47:34 +0300

    Add sound

--- clock.js
+++ clock.js
@@ -5,18 +5,20 @@ const hoursArrow = document.querySelector('.hours');
 const minutesArrow = document.querySelector('.minutes');
 const secondsArrow= document.querySelector('.seconds');
 const deg = 6;
+var playSound2 = document.getElementById("MySound2");
 
 //LET
-
 setInterval(() =>{
 
-let day = new Date();
-let hh = day.getHours() * 30; //для получения угла поворота часовой стрелки
-let mm = day.getMinutes() * deg; //для получения угла поворота минутной стрелки
-let ss = day.getSeconds() * deg; //для получения угла поворота секундной стрелки
+    let day = new Date();
+    let hh = day.getHours() * 30; //для получения угла поворота часовой стрелки
+    let mm = day.getMinutes() * deg; //для получения угла поворота минутной стрелки
+    let ss = day.getSeconds() * deg; //для получения угла поворота секундной стрелки
 
 
-hoursArrow.style.transform = `rotateZ(${(hh) + (mm / 12)}deg)`;
-minutesArrow.style.transform = `rotateZ(${mm}deg)`;
-secondsArrow.style.transform = `rotateZ(${ss}deg)`;    
+    hoursArrow.style.transform = `rotateZ(${(hh) + (mm / 12)}deg)`;
+    minutesArrow.style.transform = `rotateZ(${mm}deg)`;
+    secondsArrow.style.transform = `rotateZ(${ss}deg)`;
+
+    playSound2.play();
 });
